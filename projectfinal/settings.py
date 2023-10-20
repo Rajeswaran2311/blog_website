95102c39b48101f54c339ef694d18357802f75b4
Rajeswaran V <112332037+Rajeswaran2311@users.noreply.github.com>
2023-10-20 09:50:33 +0530

    Update settings.py

--- projectfinal/settings.py
+++ projectfinal/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = '/static/'STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_URL = 'static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
