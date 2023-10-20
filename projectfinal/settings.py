8353e25af9cb24b5f87fc190a9929549b9eccf06
Rajeswaran V <112332037+Rajeswaran2311@users.noreply.github.com>
2023-10-20 10:52:11 +0530

    Update settings.py

--- projectfinal/settings.py
+++ projectfinal/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
